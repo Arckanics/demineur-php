f2589b10035c41c3654e449a9f87ed2479d54900
Arckanics <arckanics.reiko@gmail.com>
2025-02-08 22:18:41 +0100

    Docker integration

--- vendor/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-  spl_autoload_register(function   ($class)  {
-    $file = dirname(__DIR__) . '/' . str_replace('\\', '/', $class) . '.php'; // chemin vers le dossier vendor et au format Unix (/ pour les sous-dossiers), avec un chemin relatif à la racine du document
-
-    if (file_exists($file)) {
-      require $file;
-    }
-  });
