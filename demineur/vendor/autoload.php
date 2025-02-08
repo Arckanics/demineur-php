2771b0acce796f8da965671394beada930f77bc5
Arckanics <arckanics.reiko@gmail.com>
2025-02-08 23:00:47 +0100

    autoload fixing

--- demineur/vendor/autoload.php
+++ demineur/vendor/autoload.php
@@ -1,8 +1,8 @@
 <?php
   spl_autoload_register(function   ($class)  {
-    $file = dirname(__DIR__) . 'autoload.php/' . str_replace('\\', '/', $class) . '.php'; // chemin vers le dossier vendor et au format Unix (/ pour les sous-dossiers), avec un chemin relatif à la racine du document
+    $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 
     if (file_exists($file)) {
-      require $file;
+      require_once $file;
     }
   });
