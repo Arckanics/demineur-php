66c274cfa6dcbfaf9c2f8387710068db8217ed80
Arckanics <arckanics.reiko@gmail.com>
2025-03-24 14:49:21 +0100

    rendering review

--- js/settings.js
+++ js/settings.js
@@ -7,18 +7,17 @@ export const settingsSetup = (main) => {
 	const start = frame.querySelector('#start-game');
 	
 	
-	
 	start.onclick = (e) => {
 		e.preventDefault();
 		e.stopImmediatePropagation();
 		e.stopPropagation();
 		const settings = {}
-		
+
 		inputs.map(i => {
 			const name = i.name
 			settings[name] = i.value
 		})
-		
+
 		const session = new SessionManager();
 		session.setSession()
 		session.setProperty('settings', JSON.stringify(settings));
