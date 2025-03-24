bafb6a35fb78bf6a29338c146aa1e0ce0a708a90
Arckanics <arckanics.reiko@gmail.com>
2025-03-24 14:45:23 +0100

    making fetching for server rendering

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import { settingsSetup } from "./js/settings";
+import { GameRequest } from "./js/game-request";
 
 const initDemineurGame = async ($name) => {
 	const container = document.querySelector($name);
 	
 	
+	// installing game in html
+	const reqMan = new GameRequest('/demineur')
+	
+	reqMan.send().then(req => req.text())
+	.then(res => {
+		container.innerHTML = res;
+		settingsSetup(container);
+	})
 	
 	
-	settingsSetup(container);
 };
 
 
